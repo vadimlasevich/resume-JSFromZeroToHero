a44b8686c459e7dbf808c44d9b0327811dcd9f35
vadimlasevich <lasevichv@mail.ru>
2023-01-13 05:46:14 +0300

    feat: add script slider

--- js/index.js
+++ js/index.js
@@ -3,8 +3,14 @@ const mobileMenu = document.querySelector('.header__mobile-menu');
 const mobileMenuLinks = document.querySelectorAll('.mobile-menu-link');
 const header = document.querySelector('.header');
 const shadowed = document.querySelector('.shadowed');
+
+const sliderContainer = document.querySelector('.works-slider__container');
+const sliderSlides = document.querySelectorAll('.works-slider__slide');
 const sliderBtnNext = document.querySelector('.works-slider__btn-next');
 const sliderBtnPrev = document.querySelector('.works-slider__btn-prev');
+
+let indexSlide = 0;
+let slidesLenght;
 
 const toggleMobileMenu = () => {
   burgerMenu.classList.toggle('header__burger-rotate');
@@ -19,9 +25,44 @@ const toggleMobileMenu = () => {
 
   shadowed.addEventListener('click', () => burgerMenu.click());
 };
-const rollSliderNext = () => {};
-const rollSliderPrev = () => {};
+
+const getSlidesLenght = () => {
+  if (window.innerWidth <= 520) {
+    return (slidesLenght = sliderSlides.length - 1);
+  } else {
+    return (slidesLenght = sliderSlides.length - 2);
+  }
+};
+
+const nextSlide = () => {
+  getSlidesLenght();
+
+  if (indexSlide === slidesLenght) {
+    indexSlide = 0;
+    rollSlider(indexSlide);
+  } else {
+    indexSlide++;
+    rollSlider(indexSlide);
+  }
+};
+
+const prevSlide = () => {
+  getSlidesLenght();
+
+  if (indexSlide === 0) {
+    indexSlide = slidesLenght;
+    rollSlider(indexSlide);
+  } else {
+    indexSlide--;
+    rollSlider(indexSlide);
+  }
+};
+
+const rollSlider = (index) => {
+  let widthSlide = sliderSlides[0].offsetWidth;
+  sliderContainer.style.transform = `translate(-${index * widthSlide}px)`;
+};
 
 burgerMenu.addEventListener('click', toggleMobileMenu);
-sliderBtnNext.addEventListener('click', rollSliderNext);
-sliderBtnPrev.addEventListener('click', rollSliderPrev);
+sliderBtnNext.addEventListener('click', nextSlide);
+sliderBtnPrev.addEventListener('click', prevSlide);
